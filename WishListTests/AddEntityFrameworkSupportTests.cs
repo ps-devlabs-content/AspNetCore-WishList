f4e9e5f77b417f1a4f9819c80a5ef120ceebd0ad
Jeff Hopper <jeff-hopper@pluralsight.com>
2022-06-06 15:17:59 -0500

    Fix mod3.3 test

--- WishListTests/AddEntityFrameworkSupportTests.cs
+++ WishListTests/AddEntityFrameworkSupportTests.cs
@@ -8,14 +8,14 @@ namespace WishListTests
         [Fact(DisplayName = "Configure EntityFramework @configure-entityframework")]
         public void ConfigureEntityFrameworkTest()
         {
-            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Startup.cs";
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Program.cs";
             string file;
             using (var streamReader = new StreamReader(filePath))
             {
                 file = streamReader.ReadToEnd();
             }
 
-            Assert.True(file.Contains("services.AddDbContext<ApplicationDbContext>"), "`Startup.cs`'s `Configure` did not contain a call to `ApplicationDbContext` with the `ApplicationDbContext` type.");
+            Assert.True(file.Contains("Builder.Services.AddDbContext<ApplicationDbContext>"), "`Startup.cs`'s `Configure` did not contain a call to `ApplicationDbContext` with the `ApplicationDbContext` type.");
             Assert.True(file.Contains(@"options => options.UseInMemoryDatabase"), @"`Startup.cs`'s `Configure` called `AddDbContext` but did not provide it the argument `options => options.UseInMemoryDatabase(""WishList"")`.");
         }
     }
